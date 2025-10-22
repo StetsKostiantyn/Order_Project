c5fae199c325192e1fe962eb21520dad96d74396
osmozisjones <arturoleksii@gmail.com>
2025-10-22 20:19:29 +0300

    Add project files.

---
+++ Order_Project_Tests/OrderServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using Order_Project.Models;
+using Order_Project.Services;
+using Order_Project.Services.Intefraces;
+
+namespace Order_Project_Tests
+{
+    public class OrderServiceTests
+    {
+        private readonly Mock<IInventoryService> _inventoryMock;
+        private readonly Mock<IPaymentService> _paymentMock;
+        private readonly Mock<INotificationService> _notificationMock;
+        private readonly OrderService _service;
+
+        public OrderServiceTests()
+        {
+            _inventoryMock = new Mock<IInventoryService>();
+            _paymentMock = new Mock<IPaymentService>();
+            _notificationMock = new Mock<INotificationService>();
+
+            _service = new OrderService(_inventoryMock.Object, _paymentMock.Object, _notificationMock.Object);
+        }
+
+    }
+
+}
